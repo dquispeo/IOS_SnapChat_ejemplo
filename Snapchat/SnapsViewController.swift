e690ba3c7fb27fc2310c4daa1d94ee761035533f
dquispeo <daniel.quispe.o@tecsup.edu.pe>
2020-07-02 19:32:35 -0500

    Crear Usuario

---
+++ Snapchat/SnapsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SnapsViewController.swift
+//  Snapchat
+//
+//  Created by mbtec22 on 7/2/20.
+//  Copyright © 2020 dquispe. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func cerrarSesionTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}

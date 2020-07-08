48cade8f7b50b5cec5aa65b5e31ced897fad983c
dquispeo <daniel.quispe.o@tecsup.edu.pe>
2020-07-07 19:28:20 -0500

    cambio

--- Snapchat/iniciarSesionViewController.swift
+++ Snapchat/iniciarSesionViewController.swift
@@ -27,6 +27,8 @@ class iniciarSesionViewController: UIViewController {
                         print("Tenemos el siguiente error : \(error)")
                     }else{
                         print("El usuario fue creado exitosamente")
+                        
+                        //Database.database().reference().child("usuarios").child(user!.uid).child("email").setValue(user!.email)
                         self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
                     }
                 })
